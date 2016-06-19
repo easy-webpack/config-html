688753882e1272d9506ae8844859b51dc2313eaa
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:43:58 +0200

    feat(all): initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+import * as path from 'path'
+
+/**
+ * HTML loader support for *.html
+ * Returns file content as string, loads required images.
+ *
+ * See: https://github.com/webpack/html-loader
+ */
+export function html(exclude: Array<string> = null) {
+  return function html(this: WebpackConfig): WebpackConfig {
+    return {
+      module: {
+        loaders: get(this, 'module.loaders', []).concat([{
+          test: /\.html$/,
+          loader: 'html',
+          exclude: exclude || this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : [],
+        }])
+      }
+    }
+  }
+}
